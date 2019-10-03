bba97007079f06629e43139b22fbbd9ade823e28
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2019-10-03 06:28:36 -0300

    Componente App

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+ReactDOM.render(<App />, document.getElementById('app')) //vai reenderizar na div com id=app no arquivo app.jsx
